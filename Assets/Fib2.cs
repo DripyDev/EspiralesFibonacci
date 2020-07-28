3de195019c478fddccf5e15a0b67d16ddc079184
DripyDev <doaitor@gmail.com>
2020-07-28 12:30:49 +0200

    Commit inicial
    Dos maneras diferentes de solucionar el problema de "Como generar N puntos sobre una esfera de manera uniforme" solucionado con espirales de fibonacci.

---
+++ Assets/Fib2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Fib2 : MonoBehaviour {
+    public int n;
+    private int nAux;
+    public MeshRenderer prefab;
+    
+    void Start(){
+        nAux = n;
+        InstanciarPuntos();
+    }
+    
+    private Vector3[] EsferaFibonacci2(int num) {
+        Vector3[] directions = new Vector3[num];
+        //Factor del numero aureo 1.618033989
+        float goldenRatio = (1 + Mathf.Sqrt (5)) / 2;
+        //Aumentamos el angulo del rayo en funcion de pi y del numero aureo
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+        //Calculamos el vector del rayo
+        for (int i = 0; i < n; i++) {
+            float t = (float) i / num;
+            float inclination = Mathf.Acos (1 - 2 * t);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin (inclination) * Mathf.Cos (azimuth);
+            float y = Mathf.Sin (inclination) * Mathf.Sin (azimuth);
+            float z = Mathf.Cos (inclination);
+            directions[i] = new Vector3 (x, y, z);
+        }
+        return directions;
+    }
+
+    void InstanciarPuntos () {
+        //Vector con las direcciones de los rayos en funcion del numero aureo
+        Vector3[] rayDirections = EsferaFibonacci2(n);
+        //Si un rayo NO golpea, nos movemos en esa direccion
+        for (int i = 0; i < rayDirections.Length; i++) {
+            //Posiciones corregidas para que se vean separadas
+            //Vector3 pos = Vector3.Scale(rayDirections[i], transform.localScale) + transform.position;
+            Vector3 pos = (rayDirections[i] + transform.position);
+            Quaternion q = new Quaternion(0,0,0,0);
+            Instantiate(prefab, pos, q);
+        }
+    }
+
+    void Update(){
+        if(n!=nAux){
+            LimpiarPrefabsMundo();
+            nAux=n;
+            InstanciarPuntos();
+        }
+    }
+
+    //Eliminamos todos los puntos anteriores
+    void LimpiarPrefabsMundo(){
+        GameObject[] esferas;
+        esferas = GameObject.FindGameObjectsWithTag("Fib2");
+        foreach(GameObject e in esferas){
+            Destroy(e);
+        }
+    }
+    //Funcion para alterar la n desde el correspondiente slider
+    public void CambiarN(float num){
+        n=(int) num;
+    }
+}
---
+++ Assets/FibYo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FibYo : MonoBehaviour {
+    public int n;
+    private int nAux;
+    public int valorPrueba;
+    public MeshRenderer prefab;
+    // Start is called before the first frame update
+    void Start(){
+        valorPrueba=2;
+        nAux = n;
+        InstanciarPuntos();
+    }
+
+    private Vector3[] EsferaFibonacci(int numeroPuntos){
+        Vector3[] puntos = new Vector3[numeroPuntos];
+        var goldenN = (1 + Math.Pow(5,0.5)) / 2;
+        for (int i = 0; i < numeroPuntos; i++){
+            //Cuando cambias el 2 de delante pasan cosas raras y chulas en la generacion de terreno
+            //var theta = 2 * Math.PI * (i/goldenN);
+            var theta = valorPrueba * Math.PI * (i/goldenN);
+            var phi = Math.Acos(1-2*(i+0.5f)/numeroPuntos);
+            puntos[i] = new Vector3((float) Math.Cos(theta)* (float) Math.Sin(phi), (float) Math.Sin(theta)* (float) Math.Sin(phi), (float) Math.Cos(phi));
+        }
+        return puntos;
+    }
+    void InstanciarPuntos() {
+        Vector3[] rayDirections = EsferaFibonacci(n);
+        for (int i = 0; i < rayDirections.Length; i++) {
+            //Vector3 pos = Vector3.Scale(rayDirections[i], transform.localScale) + transform.position;
+            Vector3 pos = (rayDirections[i] + transform.position);
+            Quaternion q = new Quaternion(0,0,0,0);
+            Instantiate(prefab, pos, q);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(n!=nAux){
+            LimpiarPrefabsMundo();
+            nAux=n;
+            InstanciarPuntos();
+        }
+    }
+    void LimpiarPrefabsMundo(){
+        GameObject[] esferas;
+        esferas = GameObject.FindGameObjectsWithTag("FibYo");
+        foreach(GameObject e in esferas){
+            Destroy(e);
+        }
+    }
+
+    public void CambiarN(float num){
+        n=(int) num;
+    }
+    public void CambiarvalorPrueba(float num){
+        valorPrueba=(int) num;
+    }
+}
